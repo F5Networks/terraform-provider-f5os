95618738bdb7342ef2c58dc90679058ce537c4c0
RavinderReddyF5 <r.chinthalapalli@f5.com>
2023-05-03 15:47:09 +0530

    adding tenant fixes

--- internal/provider/tenant_resource.go
+++ internal/provider/tenant_resource.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -118,13 +120,23 @@ func (r *TenantResource) Schema(ctx context.Context, req resource.SchemaRequest,
 				MarkdownDescription: "Whether crypto and compression hardware offload should be enabled on the tenant.\nWe recommend it is enabled, otherwise crypto and compression may be processed in CPU.",
 				Optional:            true,
 				Computed:            true,
+				Validators: []validator.String{
+					stringvalidator.OneOf([]string{"enabled", "disabled"}...),
+				},
 				PlanModifiers: []planmodifier.String{
-					attribute_plan_modifier.StringDefaultValue(types.StringValue("disabled"))},
+					attribute_plan_modifier.StringDefaultValue(types.StringValue("enabled"))},
 			},
 			"nodes": schema.ListAttribute{
 				MarkdownDescription: "List of integers. Specifies on which blades nodes the tenants are deployed.\nRequired for create operations.\nFor single blade platforms like rSeries only the value of 1 should be provided.",
 				Optional:            true,
+				Computed:            true,
 				ElementType:         types.Int64Type,
+				Default: listdefault.StaticValue(
+					types.ListValueMust(
+						types.Int64Type,
+						[]attr.Value{types.Int64Value(1)},
+					),
+				),
 			},
 			"vlans": schema.ListAttribute{
 				MarkdownDescription: "The existing VLAN IDs in the chassis partition that should be added to the tenant.\nThe order of these VLANs is ignored.\nThis module orders the VLANs automatically, if you deliberately re-order them in subsequent tasks, this module will not register a change.\nRequired for create operations",
@@ -178,9 +190,6 @@ func (r *TenantResource) Create(ctx context.Context, req resource.CreateRequest,
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-
 	tenantConfig := getTenantCreateConfig(ctx, req, resp)
 
 	if r.client.PlatformType == "Velos Partition" {
@@ -198,7 +207,6 @@ func (r *TenantResource) Create(ctx context.Context, req resource.CreateRequest,
 	}
 	tflog.Info(ctx, fmt.Sprintf("tenantConfig Response:%+v", string(respByte)))
 
-	// For the purposes of this example code, hardcoding a response value to
 	// save into the Terraform state.
 	data.Id = types.StringValue(data.Name.ValueString())
 
@@ -209,9 +217,6 @@ func (r *TenantResource) Create(ctx context.Context, req resource.CreateRequest,
 	}
 	tflog.Info(ctx, fmt.Sprintf("get tenantConfig :%+v", respByte2))
 	r.tenantResourceModeltoState(ctx, respByte2, data)
-	// Write logs using the tflog package
-	// Documentation: https://terraform.io/plugin/log
-	//tflog.Trace(ctx, "created a resource")
 
 	// Save data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
@@ -227,8 +232,6 @@ func (r *TenantResource) Read(ctx context.Context, req resource.ReadRequest, res
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
 	respByte, err := r.client.GetTenant(data.Name.ValueString())
 	if err != nil {
 		resp.Diagnostics.AddError("F5OS Client Error", fmt.Sprintf("Unable to Read/Get Tenants, got error: %s", err))
@@ -258,8 +261,6 @@ func (r *TenantResource) Update(ctx context.Context, req resource.UpdateRequest,
 		tenantConfig.F5TenantsTenants.Tenant[0].Config.DeploymentFile = data.DeploymentFile.ValueString()
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
 	respByte, err := r.client.UpdateTenant(tenantConfig, int(data.Timeout.ValueInt64()))
 	if err != nil {
 		resp.Diagnostics.AddError("F5OS Client Error:", fmt.Sprintf("Tenant Deploy failed, got error: %s", err))
@@ -288,8 +289,6 @@ func (r *TenantResource) Delete(ctx context.Context, req resource.DeleteRequest,
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
 	err := r.client.DeleteTenant(data.Name.ValueString())
 	if err != nil {
 		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to Delete Imported Image, got error: %s", err))
@@ -309,29 +308,8 @@ func (r *TenantResource) tenantResourceModeltoState(ctx context.Context, respDat
 	data.MgmtGateway = types.StringValue(respData.F5TenantsTenant[0].State.Gateway)
 	data.Status = types.StringValue(respData.F5TenantsTenant[0].State.Status)
 	data.VirtualdiskSize = types.Int64Value(int64(respData.F5TenantsTenant[0].Config.Storage.Size))
+	data.Cryptos = types.StringValue(respData.F5TenantsTenant[0].Config.Cryptos)
 }
-
-// func getTenantCreatebackupConfig(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) *f5ossdk.TenantObj {
-//	var data *TenantResourceModel
-//
-//	// Read Terraform plan data into the model
-//	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-//
-//	tenantConfig := &f5ossdk.TenantObj{}
-//	tenantConfig.Name = data.Name.ValueString()
-//	tenantSubConfig := f5ossdk.TenantConfig{}
-//	tenantSubConfig.Image = data.ImageName.ValueString()
-//	tenantSubConfig.MgmtIp = data.MgmtIP.ValueString()
-//	tenantSubConfig.Nodes = []int{1}
-//	tenantSubConfig.Gateway = data.MgmtGateway.ValueString()
-//	tenantSubConfig.PrefixLength = int(data.MgmtPrefix.ValueInt64())
-//	tenantSubConfig.VcpuCoresPerNode = int(data.CpuCores.ValueInt64())
-//	tenantSubConfig.Memory = 3 * 1024 * int(data.CpuCores.ValueInt64())
-//	tenantSubConfig.RunningState = data.RunningState.String()
-//	data.Vlans.ElementsAs(ctx, &tenantSubConfig.Vlans, false)
-//	tenantConfig.Config = tenantSubConfig
-//	return tenantConfig
-// }
 
 func getTenantCreateConfig(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) *f5ossdk.TenantsObj {
 	var data *TenantResourceModel
@@ -348,10 +326,11 @@ func getTenantCreateConfig(ctx context.Context, req resource.CreateRequest, resp
 	tenantSubbj.Config.PrefixLength = int(data.MgmtPrefix.ValueInt64())
 	tenantSubbj.Config.VcpuCoresPerNode = int(data.CpuCores.ValueInt64())
 	tenantSubbj.Config.Memory = 3 * 1024 * int(data.CpuCores.ValueInt64())
-	data.Vlans.ElementsAs(ctx, tenantSubbj.Config.Vlans, false)
+	data.Vlans.ElementsAs(ctx, &tenantSubbj.Config.Vlans, false)
 	tenantSubbj.Config.PrefixLength = int(data.MgmtPrefix.ValueInt64())
 	tenantSubbj.Config.RunningState = data.RunningState.ValueString()
-	tenantSubbj.Config.Nodes = []int{1}
+	tenantSubbj.Config.Cryptos = data.Cryptos.ValueString()
+	data.Nodes.ElementsAs(ctx, &tenantSubbj.Config.Nodes, false)
 	tenantSubbj.Config.Storage.Size = int(data.VirtualdiskSize.ValueInt64())
 
 	tenantConfig := new(f5ossdk.TenantsObj)
@@ -374,10 +353,11 @@ func getTenantUpdateConfig(ctx context.Context, req resource.UpdateRequest, resp
 	tenantSubbj.Config.PrefixLength = int(data.MgmtPrefix.ValueInt64())
 	tenantSubbj.Config.VcpuCoresPerNode = int(data.CpuCores.ValueInt64())
 	tenantSubbj.Config.Memory = 3 * 1024 * int(data.CpuCores.ValueInt64())
-	data.Vlans.ElementsAs(ctx, tenantSubbj.Config.Vlans, false)
+	data.Nodes.ElementsAs(ctx, &tenantSubbj.Config.Nodes, false)
+	data.Vlans.ElementsAs(ctx, &tenantSubbj.Config.Vlans, false)
 	tenantSubbj.Config.PrefixLength = int(data.MgmtPrefix.ValueInt64())
 	tenantSubbj.Config.RunningState = data.RunningState.ValueString()
-	tenantSubbj.Config.Nodes = []int{1}
+	tenantSubbj.Config.Cryptos = data.Cryptos.ValueString()
 	tenantSubbj.Config.Storage.Size = int(data.VirtualdiskSize.ValueInt64())
 
 	tenantpatchConfig := new(f5ossdk.TenantsPatchObj)
