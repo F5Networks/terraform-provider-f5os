422bf51587f0a9192212a92cfb5288b894b9322d
chinthalapalli <r.chinthalapalli@f5.com>
2024-03-28 11:18:26 +0530

    adding devel changes

--- internal/provider/tenant_image_data_source.go
+++ internal/provider/tenant_image_data_source.go
@@ -3,6 +3,8 @@ package provider
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
@@ -76,16 +78,37 @@ func (d *ImageInfoDataSource) Read(ctx context.Context, req datasource.ReadReque
 	if resp.Diagnostics.HasError() {
 		return
 	}
-	imageObj, err := d.client.GetImage(data.ImageName.ValueString())
-	if err != nil {
-		resp.Diagnostics.AddError("Unable to Get Image Details", fmt.Sprintf("Error:%s", err))
-		return
-	}
 	var availableFlag = true
-	for _, val := range imageObj.TenantImages {
-		if val.Name == data.ImageName.ValueString() && val.Status == "not-present" {
-			availableFlag = false
+	timeBefore := time.Now().Add(6 * time.Minute)
+	for time.Now().Before(timeBefore) {
+		availableFlag = false
+		imageObj, err := d.client.GetImage(data.ImageName.ValueString())
+		if err != nil {
+			resp.Diagnostics.AddError("Unable to Get Image Details", fmt.Sprintf("Error:%s", err))
+			return
 		}
+		for _, val := range imageObj.TenantImages {
+			log.Printf("[DEBUG] Image Status: %+v", val.Status)
+			if val.Name == data.ImageName.ValueString() {
+				if val.Status == "not-present" || val.Status == "processing" {
+					availableFlag = false
+				}
+				if val.Status == "replicated" || val.Status == "processed" {
+					diff := time.Until(timeBefore)
+					if !(diff > 2*time.Minute) {
+						time.Sleep(2 * time.Minute)
+					}
+					availableFlag = true
+					data.ImageName = types.StringValue(val.Name)
+					data.ImageStatus = types.StringValue(val.Status)
+					break
+				}
+			}
+		}
+		if availableFlag {
+			break
+		}
+		time.Sleep(2 * time.Minute)
 	}
 	if !availableFlag {
 		resp.Diagnostics.AddError("Unable to Get Image Details", fmt.Sprintf("Get Image: %s failed with error:%s", data.ImageName.ValueString(), "not-present"))
@@ -93,10 +116,7 @@ func (d *ImageInfoDataSource) Read(ctx context.Context, req datasource.ReadReque
 	}
 
 	data.ID = types.StringValue(data.ImageName.ValueString())
-	data.ImageName = types.StringValue(imageObj.TenantImages[0].Name)
-	data.ImageStatus = types.StringValue(imageObj.TenantImages[0].Status)
 	teemData.ResourceName = "f5os_tenant_image"
-
 	// Save data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
