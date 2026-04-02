0903c6d1dc0f70eadeb6bf047f83b6ea4194d4b7
Greg Crosby <g.crosby@f5.com>
2026-04-02 13:23:09 -0700

    chore: update release script to include templates folder

--- update_github.sh
+++ update_github.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 rm -rf /tmp/terraform-provider-f5os
 git clone --depth 1 https://github.com/F5Networks/terraform-provider-f5os.git /tmp/terraform-provider-f5os
-cp -rf docs examples internal vendor go.mod go.sum .gitignore main.go CHANGELOG.md /tmp/terraform-provider-f5os
+cp -rf docs examples internal vendor templates go.mod go.sum .gitignore main.go CHANGELOG.md /tmp/terraform-provider-f5os
